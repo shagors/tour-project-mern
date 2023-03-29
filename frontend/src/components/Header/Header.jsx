2e48737a1d11921239f4b5da55040c070fe35915
shajjad Shagor <shajjadshagor@gmail.com>
2023-03-29 22:53:03 +0600

    logo add and navbar design

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Col, Container, Row } from "reactstrap";
-import { NavLink } from "react-router-dom";
+import { Button, Col, Container, Row } from "reactstrap";
+import { Link, NavLink } from "react-router-dom";
 import "./header.css";
 import logo from "../../assets/images/logo.png";
 
@@ -10,7 +10,7 @@ const nav__links = [
     display: "Home",
   },
   {
-    path: "#",
+    path: "/about",
     display: "About",
   },
   {
@@ -35,12 +35,34 @@ const Header = () => {
               <ul className="menu d-flex align-items-center gap-5">
                 {nav__links.map((item, index) => (
                   <li className="nav__item" key={index}>
-                    <NavLink to={item.path}>{item.display}</NavLink>
+                    <NavLink
+                      to={item.path}
+                      className={(navClass) =>
+                        navClass.isActive ? "active__link" : ""
+                      }
+                    >
+                      {item.display}
+                    </NavLink>
                   </li>
                 ))}
               </ul>
             </div>
             {/* ============= menu end ================== */}
+
+            <div className="nav__right d-flex align-items-center gap-4">
+              <div className="nav__btns d-flex align-items-center gap-4">
+                <Button className="btn secondary__btn">
+                  <Link to="/login">Login</Link>
+                </Button>
+                <Button className="btn primary__btn">
+                  <Link to="/register">Register</Link>
+                </Button>
+              </div>
+
+              <span className="mobile__menu">
+                <i className="ri-menu-line"></i>
+              </span>
+            </div>
           </div>
         </Row>
       </Container>
